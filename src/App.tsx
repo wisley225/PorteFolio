6ef24796086efa7164bd5495e0356eb8cc7f9870
LEWIS GUEI <lewisguei67@gmail.com>
2025-06-27 18:47:00 +0000

    debut du projet

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          React 18 + Vite + Tailwind CSS 3.4.17 prêt à coder !
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App
